0a5e04b3919d7752511d26ad50134154f462d53a
Austin Curry <austin.curry360@gmail.com>
2018-04-28 00:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ return `It's 20 days until Mother's Day!`
 function happyHolidayTo(thanksgiving, sister) {
 return `Happy Independence Day, you!`
 }
+function happyHolidaysTo(name) {
+return `Happy holidays, you!`
+}
